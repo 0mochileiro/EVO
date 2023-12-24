0eaa9ed5349c7a34ed68d5b82ce19f7ea55f8e8e
Joao Mantovani <84257811+0mochileiro@users.noreply.github.com>
2023-12-24 01:58:12 -0300

    Update project "EVO.ApiService".
    Update project "EVO.ApiService" - Implement CORS policies for development environment.

--- EVO/EVO.ApiService/Program.cs
+++ EVO/EVO.ApiService/Program.cs
@@ -1,8 +1,22 @@
-using Microsoft.EntityFrameworkCore;    
+using Microsoft.EntityFrameworkCore;
 using EVO.Repository.Data;
 using EVO.Common.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
+
+if (builder.Environment.IsDevelopment())
+{
+    // Add 'EnvironmentDevelopmentPolicy' to handler CORS.
+    builder.Services.AddCors(cors =>
+    {
+        cors.AddPolicy("EnvironmentDevelopmentPolicy", builder =>
+        {
+            builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+    });
+}
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -17,6 +31,14 @@ builder.Services.AddDbContext<Context>(options =>
 
 var app = builder.Build();
 
+if (app.Environment.IsDevelopment())
+{
+    // Use policy 'EnvironmentDevelopmentPolicy' to handle CORS.
+    app.UseCors("EnvironmentDevelopmentPolicy");
+}
+
 app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
